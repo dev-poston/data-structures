2e1bd3ce14818e1504914886c90cd929bf877974
Devon Poston <dev.poston@gmail.com>
2020-12-16 13:16:34 -0800

    complete pseudoclassical stack and queue

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.counter = 0;
+  this.storage = {};
 };
 
+Stack.prototype.push = function (value) {
+  this.counter++;
+  this.storage[this.counter] = value;
+};
+
+Stack.prototype.pop = function () {
+  if (this.counter > 0) {
+    var topper = this.storage[this.counter];
+    delete this.storage[this.counter];
+    this.counter--;
+    return topper;
+  }
+};
+
+Stack.prototype.size = function () {
+  return this.counter;
+};
 
