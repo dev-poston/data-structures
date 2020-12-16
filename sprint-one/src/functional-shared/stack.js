ccf4f2ddfaf3dc0058247b2c5f5179a0e7467682
Devon Poston <dev.poston@gmail.com>
2020-12-15 23:36:40 -0800

    functional shared queue complete, stack almost complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = {};
+  obj.storage = {};
+  obj.size = 0;
+  _.extend(obj, stackMethods);
+  return obj;
 };
 
-var stackMethods = {};
-
-
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.size] = value;
+    this.size++;
+  },
+  pop: function() {
+    if (this.size > 0) {
+      var topper = this.storage[this.size];
+      delete this.storage[this.size];
+      this.size--;
+      return topper;
+    }
+  },
+  size: function() {
+    return this.size;
+  }
+};
