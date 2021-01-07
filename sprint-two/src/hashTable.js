75b04faa8eb2b5e54fcee5568dbe8dd3d3b3d59d
Devon Poston <dev.poston@gmail.com>
2021-01-06 21:56:25 -0800

    complete hashtable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,46 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = [];
+  var tuple = {};
+  tuple[k] = v;
+  bucket.push(tuple);
+
+  if (this._storage[index]) {
+    for (var i = 0; i < this._storage[index].length; i++) {
+      if (this._storage[index][i][k]) {
+        this._storage[index][i][k] = v;
+      } else {
+        this._storage[index].push(tuple);
+      }
+    }
+  } else {
+    this._storage[index] = bucket;
+  }
+  console.log(this._storage);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage[index];
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][k]) {
+      return bucket[i][k];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  var bucket = this._storage[index];
+  for (var i = 0; i < bucket.length; i++) {
+    if (bucket[i][k]) {
+      delete bucket[i][k];
+    }
+  }
 };
 
 
